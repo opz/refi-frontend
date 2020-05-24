37b33455eb677c6a2f91abf96c18df0971c694b2
svetamedved <sveta.berrios@gmail.com>
2020-05-24 11:21:29 -0400

    installed all dependencies, created components folder, created identicon and network_status files, added jazzicon to the identicon component, modidied App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Portis from "@portis/web3";
+import Fortmatic from "fortmatic";
+import Squarelink from "squarelink";
+import Torus from "@toruslabs/torus-embed";
+import Authereum from "authereum";
+import BurnerConnectProvider from "@burner-wallet/burner-connect-provider";
+
 import './App.css';
+import IdentIcon from './components/identicon';
+
+// const providerOptions = {
+//   /* See Provider Options Section */
+// };
+
+// const web3Modal = new Web3Modal({
+//   network: "mainnet", // optional
+//   cacheProvider: true, // optional
+//   providerOptions // required
+// });
+
+// const provider = await web3Modal.connect();
+
+// const web3 = new Web3(provider);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Hello world </h1>
+      <IdentIcon />
     </div>
   );
 }
