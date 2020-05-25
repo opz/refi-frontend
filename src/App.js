9cb5b75c4e4707c9454de9a5e1cde2aa562bbce4
svetamedved <sveta.berrios@gmail.com>
2020-05-25 14:54:20 -0400

    installed materialUI, created header component and connected account id to the header component through app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from "web3";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
@@ -8,30 +8,44 @@ import Squarelink from "squarelink";
 import Torus from "@toruslabs/torus-embed";
 import Authereum from "authereum";
 import BurnerConnectProvider from "@burner-wallet/burner-connect-provider";
-import './App.css';
 import IdentIcon from './components/identicon';
+import Container from '@material-ui/core/Container';
+import Header from './components/header';
 
-// const providerOptions = {
-//   /* See Provider Options Section */
-// };
-
-// const web3Modal = new Web3Modal({
-//   network: "mainnet", // optional
-//   cacheProvider: true, // optional
-//   providerOptions // required
-// });
-
-// const provider = await web3Modal.connect();
-
-// const web3 = new Web3(provider);
+const providerOptions = {};
+const web3Modal = new Web3Modal({
+  network: "mainnet", // optional
+  cacheProvider: true, // optional
+  providerOptions // required
+});
 
 function App() {
+  const [account, setAccount] = useState('');
+  async function connect() {
+    try {
+      const provider = await web3Modal.connect();
+      const web3 = new Web3(provider);
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    if (window.web3) {
+      connect();
+    }
+  }, []);
+
   return (
-    <div>
-      <h1> Hello world </h1>
-      <IdentIcon />
-    </div>
+    <Container maxWidth="sm">
+      <Header account={account}/>
+    </Container>
   );
 }
 
 export default App;
+
+
+
