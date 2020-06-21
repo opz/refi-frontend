90c370020a8ef89dfea6cc59862f6ec6b7d330f8
svetamedved <sveta.berrios@gmail.com>
2020-06-21 18:25:54 -0400

    created aave instances (ABI, address)

--- src/providers/wallet.js
+++ src/providers/wallet.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
-
+import providerAbi from "refi-core/lib/aave-protocol/abi/LendingPoolAddressesProvider.json";
+import { LendingPoolAddressesProvider as providerAddress } from "refi-core/config/aave-protocol/kovan.json";
 
 const WalletContext = React.createContext();
 const WalletConsumer = WalletContext.Consumer;
@@ -8,6 +9,7 @@ const WalletConsumer = WalletContext.Consumer;
 const WalletProvider = props => {
   const [wallet, setWallet] = useState(null);
   const [auth, setAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [account, setAccount] = useState('');
 
   async function loadAccount() {
@@ -19,9 +21,27 @@ const WalletProvider = props => {
     }
   }
 
+  async function fetchContracts() {
+  const provider = new wallet.eth.Contract(providerAbi, providerAddress);
+  // Get the latest LendingPool contract address
+  const lpAddress = await provider.methods
+      .getLendingPool()
+      .call()
+      .catch((e) => {
+          throw Error(`Error getting lendingPool address: ${e.message}`)
+      })
+      alert(lpAddress)
+  setTimeout(() => {
+    setLoading(false)
+  }, 3000)
+  return []
+};
+
+
   useEffect(() => {
     if (wallet) {
       loadAccount();
+      fetchContracts();
     } else {
       setAccount('');
     }
